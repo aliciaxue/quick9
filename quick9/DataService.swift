93450a904ddbee32da3b2f7a9a510c4e7c9094a4
xul4284g <u3512485@connect.hku.hk>
2016-06-30 00:52:51 +0800

    Add DataService

--- quick9/DataService.swift
+++ quick9/DataService.swift
@@ -8,10 +8,19 @@
 
 import Foundation
 import Firebase
+import FirebaseDatabase
 
 class DataService {
     static let ds = DataService()
     
-    private var _REF_BASE = Firebase(url: "https://quick9-7dce5.firebaseio.com/")
+    private var _REF_BASE = FIRDatabase.database().reference()
+    private var _REF_USERS = FIRDatabase.database().reference().child("users")
+    
+    var REF_BASE: FIRDatabaseReference {
+        return _REF_BASE
+    }
+    var REF_USERS: FIRDatabaseReference {
+        return _REF_USERS
+    }
     
 }
