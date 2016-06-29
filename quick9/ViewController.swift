a3008196b625cd57d9192c6462d3ffb497c0d095
xul4284g <u3512485@connect.hku.hk>
2016-06-29 13:59:48 +0800

    Initial Commit

---
+++ quick9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quick9
+//
+//  Created by LiaoPeiyu on 6/29/16.
+//  Copyright © 2016 IOException. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
