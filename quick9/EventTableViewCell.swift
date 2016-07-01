cf2bf30a328593c49a13face8633b210c58f996e
xul4284g <u3512485@connect.hku.hk>
2016-07-01 14:29:55 +0800

    Modify UserProfileInfo.swift

---
+++ quick9/EventTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  EventTableViewCell.swift
+//  quick9
+//
+//  Created by LiaoPeiyu on 7/1/16.
+//  Copyright © 2016 IOException. All rights reserved.
+//
+
+import Cocoa
+
+class EventTableViewCell: UITableViewCell {
+
+}
--- quick9/UserProfileInfo.swift
+++ quick9/UserProfileInfo.swift
@@ -12,22 +12,94 @@ class UserProfileInfo {
     
     // MARK: Properties
     private var _realName: String
-    private var _nickName: String?
+    private var _nickName: String
     private var _birthday: NSDate
     private var _gender:GenderType
-    private var _address: String?
-    private var _city: String?
-    private var _district: String?
-    private var _mobile: String?
-    private var _email: String?
+    private var _address: String
+    private var _city: String
+    private var _district: String
+    private var _mobile: String
+    private var _email: String
+    private var _imageUrl: String
 
+    var realName: String {
+        get {
+            return _realName
+        } set {
+            _realName = newValue
+        }
+    }
+    var nickName: String {
+        get {
+            return _nickName
+        } set {
+            _nickName = newValue
+        }
+    }
+    var birthday: NSDate {
+        get {
+            return _birthday
+        } set {
+            _birthday = newValue
+        }
+    }
+    var gender: GenderType {
+        get {
+            return _gender
+        } set {
+            _gender = newValue
+        }
+    }
+    var address: String{
+        get {
+            return _address
+        } set {
+            _address = newValue
+        }
+    }
+    var city: String {
+        get {
+            return _city
+        } set {
+            _city = newValue
+        }
+    }
+    var district: String {
+        get {
+            return _district
+        } set {
+            _district = newValue
+        }
+    }
+    var mobile: String {
+        get {
+            return _mobile
+        } set {
+            _mobile = newValue
+        }
+    }
+    var email: String {
+        get {
+            return _email
+        } set {
+            _email = newValue
+        }
+    }
+    var imageUrl: String {
+        get {
+            return _imageUrl
+        } set {
+            _imageUrl = newValue
+        }
+    }
     var age: Int {
         return getAge()
     }
     
+    
     // MARK: Functions
     
-    init (realName: String, nickName: String?, birthday: NSDate, gender:GenderType, address: String?, city: String?, district: String?, mobile: String?, email: String?){
+    init (realName: String, nickName: String?, birthday: NSDate, gender:GenderType, address: String?, city: String?, district: String?, mobile: String?, email: String?, imageUrl: String?){
         _realName = realName
         _nickName = nickName ?? realName
         _birthday = birthday
@@ -37,6 +109,7 @@ class UserProfileInfo {
         _district = district ?? ""
         _mobile = mobile ?? ""
         _email = email ?? ""
+        _imageUrl = imageUrl ?? ""
     }
     
     func getAge()-> Int {
